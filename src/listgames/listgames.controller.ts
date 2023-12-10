639eec7a082c1b9fba5ecec398eacba9d4a0203f
DIEGO ANTONIO BARROS TOBAR <diego.barros@alumnos.ucn.cl>
2023-12-10 19:55:21 -0300

    refactor: change method private to public

--- src/listgames/listgames.controller.ts
+++ src/listgames/listgames.controller.ts
@@ -18,25 +18,25 @@ export class ListgamesController {
   private readonly service: ListgamesService;
 
   @GrpcMethod(LIST_GAMES_SERVICE_NAME, 'createListGames')
-  private async createListGames(
+  public async createListGames(
     data: ListGamesRequestDto,
   ): Promise<CreateListGamesResponse> {
     return await this.service.createListGames(data);
   }
   @GrpcMethod(LIST_GAMES_SERVICE_NAME, 'addGamesToList')
-  private async addGamesToList(
+  public async addGamesToList(
     data: AddGamesToListRequestDto,
   ): Promise<AddGamesToListResponse> {
     return await this.service.addGameToList(data.listId, data.gameIds);
   }
   @GrpcMethod(LIST_GAMES_SERVICE_NAME, 'getGamesInList')
-  private async getGamesInList(
+  public async getGamesInList(
     data: GetGamesInListRequest,
   ): Promise<GetGamesInListResponse> {
     return await this.service.getGamesInList(data.listId);
   }
   @GrpcMethod(LIST_GAMES_SERVICE_NAME, 'deleteGameFromList')
-  private async deleteGameFromList(
+  public async deleteGameFromList(
     data: DeleteGameFromListRequest,
   ): Promise<DeleteGameFromListResponse> {
     return await this.service.deleteGameFromList(data.listId, data.gameId);
